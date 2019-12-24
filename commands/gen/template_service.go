6ae83b3b3a00f4f56fa0a889fd4a2cfcc267271f
sunmoon <i@liming.me>
2019-12-24 15:28:10 +0800

    优化模板

--- commands/gen/template_service.go
+++ commands/gen/template_service.go
@@ -154,7 +154,7 @@ package {tableName}
 import (
 	"errors"
 	"{moduleName}/app/model/{tableName}"
-    "{moduleName}/app/model/service"
+    "{moduleName}/app/service"
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
 	"github.com/gogf/gf/util/gconv"
